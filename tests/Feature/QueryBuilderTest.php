e83c52e9afe688b6d5d735a6239ea56095eab15e
Karungg <miftahfadilah71@gmail.com>
2023-09-24 16:42:15 +0700

    Query Builder Select

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 use function PHPUnit\Framework\assertTrue;
@@ -30,5 +31,17 @@ class QueryBuilderTest extends TestCase
 
         $results = DB::select('SELECT COUNT(id) as total FROM categories');
         self::assertEquals(2, $results[0]->total);
+    }
+
+    public function testSelect()
+    {
+        $this->testInsert();
+
+        $collection = DB::table('categories')->select(['id', 'name'])->get();
+        self::assertNotNull($collection);
+
+        $collection->each(function ($item) {
+            Log::info(json_encode($item));
+        });
     }
 }
